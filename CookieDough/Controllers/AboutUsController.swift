8fd55bbc874e2453f57a92742fd7d845074b1764
Zohaib Jamshaid <zohaib.jamshaid@upgenicsint.com>
2019-05-21 16:01:16 +0500

    about us copy paste

--- CookieDough/Controllers/AboutUsController.swift
+++ CookieDough/Controllers/AboutUsController.swift
@@ -9,6 +9,57 @@
 import Foundation
 import UIKit
 
+
+
+class KGCopyableLabel: UILabel {
+    
+    override public var canBecomeFirstResponder: Bool {
+        get {
+            return true
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        isUserInteractionEnabled = true
+        addGestureRecognizer(UILongPressGestureRecognizer(
+            target: self,
+            action: #selector(showCopyMenu(sender:))
+        ))
+    }
+    
+    override func copy(_ sender: Any?) {
+        UIPasteboard.general.string = text
+        UIMenuController.shared.setMenuVisible(false, animated: true)
+    }
+    
+    @objc func showCopyMenu(sender: Any?) {
+        becomeFirstResponder()
+        let menu = UIMenuController.shared
+        if !menu.isMenuVisible {
+            menu.setTargetRect(bounds, in: self)
+            menu.setMenuVisible(true, animated: true)
+        }
+    }
+    
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        return (action == #selector(copy(_:)))
+    }
+}
+
+
+
+
+
 class AboutUsController: UIViewController {
     @IBOutlet weak var cookieDoughTitle: UILabel!
     @IBOutlet weak var aboutUsDetail: UILabel!
@@ -111,3 +162,8 @@ extension UIImageView {
         self.tintColor = color
     }
 }
+
+
+
+
+
